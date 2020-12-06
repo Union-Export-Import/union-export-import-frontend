09843a2e17bcb6757436af6073a4cb8528ab99bb
AungMyoOo <dev.aungmyooo2015@gmail.com>
2020-12-06 20:57:22 +0630

    change pwd ui & reset dialog

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -11,14 +11,39 @@
       <el-col :span="12">
         <div class="login-form">
           <h3>Login</h3>
-          <p class="login-contact">Don’t have an account? Please contact your <strong>Admin</strong></p>
+          <p class="login-contact">
+            Don’t have an account? Please contact your <strong>Admin</strong>
+          </p>
           <div class="form-input">
             <input placeholder="Please enter your email here" />
           </div>
           <div class="form-input">
-            <input type="password" placeholder="Please enter your password here" />
+            <input
+              type="password"
+              placeholder="Please enter your password here"
+            />
           </div>
-          <p class="forget-password">Forget Password?</p>
+          <p class="forget-password" @click="dialogVisible = true">
+            Forget Password?
+          </p>
+          <el-dialog
+            title="Reset Password"
+            v-model="dialogVisible"
+            width="50%"
+            :before-close="handleClose"
+          >
+            <div class="form-input">
+              <input placeholder="Please enter your email here" />
+            </div>
+            <template #footer>
+              <span class="dialog-footer">
+                <el-button class="reset-cancel-button" @click="dialogVisible = false">Cancel</el-button>
+                <el-button class="reset-password-button" type="primary" @click="dialogVisible = false"
+                  >Submit</el-button
+                >
+              </span>
+            </template>
+          </el-dialog>
           <button class="login-button">LOGIN</button>
         </div>
       </el-col>
@@ -29,7 +54,14 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      dialogVisible: false,
+    };
+  },
+  methods: {
+    handleClose() {
+      this.dialogVisible = false;
+    },
   },
 };
 </script>
